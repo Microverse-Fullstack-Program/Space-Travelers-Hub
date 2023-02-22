12114fdf3cac24c53483ab6d32c0e4dca540028e
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-22 09:34:53 +0300

    Update App.js and index.js components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-import Profile from './pages/Profile';
-import Dragons from './pages/Dragons';
-import Missions from './pages/Missions';
-import Rockets from './pages/Rockets';
+import Profile from './components/Profile';
+import Dragons from './components/Dragons';
+import Missions from './components/Missions';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
     <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/dragons" element={<Dragons />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/dragons" element={<Dragons />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   );
 }
