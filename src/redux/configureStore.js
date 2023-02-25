bed8dbc8517aef43759e0eeff24a0c8732626dc2
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-26 01:19:16 +0300

    Add root reducer - a combiner

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rocketsReducer from './rockets/rockets';
 import dragonsReducer from './dragons/dragons';
 import missionsReducer from './missions/missions';
 
-const store = configureStore({
-  reducer: {
-    rockets: rocketsReducer,
-    dragons: dragonsReducer,
-    missions: missionsReducer,
-  },
+const MyMiddlewares = [thunk, logger];
+const rootReducer = combineReducers({
+  rockets: rocketsReducer,
+  dragons: dragonsReducer,
+  missions: missionsReducer,
 });
+
+const store = configureStore({
+  reducer: rootReducer,
+}, applyMiddleware(...MyMiddlewares));
 
 export default store;
