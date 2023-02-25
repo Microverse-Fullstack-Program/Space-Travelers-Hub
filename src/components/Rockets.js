c600188326fa57554e69e2bbb5886f33b57c47e5
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-25 13:05:45 +0300

    Add fetch dispatch for Rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,35 @@
-const Rockets = () => {
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useRef } from 'react';
+import RocketsPage from '../pages/RocketsPage';
+import { FetchRockets } from '../redux/rockets/rockets';
 
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const shouldFetchRockets = useRef(true);
+  useEffect(() => {
+    if (shouldFetchRockets.current) {
+      shouldFetchRockets.current = false;
+      dispatch(FetchRockets());
+    }
+  }, [dispatch]);
+
+  let rocketList = useSelector((state) => state.rockets).rockets;
+  if (rocketList === null || rocketList === undefined) {
+    rocketList = [];
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(rocketList);
+  }
+
+  return (
+    <>
+      {rocketList.map((rkt) => (
+        <div key={rkt.id}>
+          <RocketsPage rocket={rkt} />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Rockets;
