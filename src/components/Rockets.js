779afaa15ba8f29b942ee5c6b2a3721de9b24278
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-26 01:20:05 +0300

    Write actions and reducers for booking rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,22 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import RocketsPage from '../pages/RocketsPage';
 import { FetchRockets } from '../redux/rockets/rockets';
 
 const Rockets = () => {
+  const rocketList = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
-  const shouldFetchRockets = useRef(true);
-  useEffect(() => {
-    if (shouldFetchRockets.current) {
-      shouldFetchRockets.current = false;
-      dispatch(FetchRockets());
-    }
-  }, [dispatch]);
 
-  let rocketList = useSelector((state) => state.rockets).rockets;
-  if (rocketList === null || rocketList === undefined) {
-    rocketList = [];
-  }
+  useEffect(() => {
+    if (!rocketList.length) dispatch(FetchRockets);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
 
   return (
     <div className="wrapper">
