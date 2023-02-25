4575ba47c3bc288b12187567d61fbe866304ef2d
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-26 01:40:00 +0300

    Implement dragon booking and cancellation

--- src/pages/DragonsPage.js
+++ src/pages/DragonsPage.js
@@ -1,28 +1,56 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { ReserveDragon, UnreserveDragon } from '../redux/dragons/dragons';
 
-const DragonsPage = ({ dragon }) => (
-  <>
-    <img src={dragon.image} alt="dragon" />
-    <div className="drg-detail">
-      <h2 className="drg-title">
-        {' '}
-        {dragon.dragon_name}
-        {' '}
-        (
-        {dragon.type}
-        )
-      </h2>
-      <p className="drg-desc">
-        {' '}
-        {dragon.description}
-        {' '}
-      </p>
-      <button type="button" className="reserveDrgBtn">
-        Reserve Dragon
-      </button>
-    </div>
-  </>
-);
+const DragonsPage = ({ dragon }) => {
+  const dispatch = useDispatch();
+
+  const handleReserve = (id) => {
+    dispatch(ReserveDragon(id));
+  };
+
+  const handleCancel = (id) => {
+    dispatch(UnreserveDragon(id));
+  };
+
+  return (
+    <>
+      <img src={dragon.image} alt="dragon" />
+      <div className="drg-detail">
+        <h2 className="drg-title">
+          {' '}
+          {dragon.dragon_name}
+          {' '}
+          (
+          {dragon.type}
+          )
+        </h2>
+        <p>
+          <span className="reserve-badge">{(dragon.reserve ? '(Reserved)' : '')}</span>
+        </p>
+        <p className="drg-desc">
+          {' '}
+          {dragon.description}
+          {' '}
+        </p>
+        <button
+          type="button"
+          className={`rocket-btn ${dragon.reserve ? 'hide' : ''}`}
+          onClick={() => handleReserve(dragon.id)}
+        >
+          Reserve Dragon
+        </button>
+        <button
+          type="button"
+          className={`rocket-btn close ${dragon.reserve ? '' : 'hide'}`}
+          onClick={() => handleCancel(dragon.id)}
+        >
+          Cancel Dragon
+        </button>
+      </div>
+    </>
+  );
+};
 
 DragonsPage.propTypes = {
   dragon: PropTypes.shape({
@@ -31,7 +59,7 @@ DragonsPage.propTypes = {
     type: PropTypes.string,
     description: PropTypes.string,
     image: PropTypes.string,
-    active: PropTypes.bool,
+    reserve: PropTypes.bool,
   }).isRequired,
 };
 
